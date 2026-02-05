def6a0f6600f5671bd96477bb1fe6c58944ec177
Thomas Jay Rush <jrush@quickblocks.io>
2026-02-05 17:55:03 -0500

    Cleaning

--- references/mockups/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { StatusBadge } from './StatusBadge';
-export { QualityBadge } from './QualityBadge';
-export { ResponseBadge } from './ResponseBadge';
-export { KeyboardHints } from './KeyboardHints';
--- references/mockups/src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-export { CollectionsPage } from './CollectionsPage';
-export { WorksPage } from './WorksPage';
-export { WorkDetailPage } from './WorkDetailPage';
-export { SubmissionsPage } from './SubmissionsPage';
-export { OrganizationsPage } from './OrganizationsPage';
